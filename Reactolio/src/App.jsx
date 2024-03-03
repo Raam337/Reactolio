47ddbe8b95ac2f5ef9f9558c51f4b38d42625078
RobF <raam337@gmail.com>
2024-03-03 20:22:15 +0000

    Initial commit and navbar

---
+++ Reactolio/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {MainNavBar} from "./Components"
+
+
+function App() {
+
+  return (
+  <>
+    <MainNavBar />
+  </>
+  )
+}
+
+export default App
