7be46aa03c5cbaf84c45b7b64b43c34dd10dba69
RobF <raam337@gmail.com>
2024-03-05 16:28:11 +0000

    Contact grid

--- Reactolio/src/Components/Contact/index.jsx
+++ Reactolio/src/Components/Contact/index.jsx
@@ -2,23 +2,31 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/esm/Container'
 import contacts from "../../assets/contact.json"
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import "./style.css"
 
 function Contact(){
+
     return (
-    <Container>
+    <Container className="d-flex flex-row my-5 justify-content-center">
+        <Row className='row-gap-3 gap-3' xs={1} md={2} lg={4}>
         {contacts.map( (item)=>{
-            return <InfoCard key={item.name} ico={item.ico} name={item.name} action={item.action} link={item.link} />
-        })}
+            return (
+            <Col>
+              <InfoCard key={item.name} ico={item.ico} name={item.name} action={item.action} link={item.link} />
+            </Col>
+            )
+          })}
+        </Row>
     </Container>)
 }
 
 
 function InfoCard(props) {
   return (
-    <Card style={{ width: '18rem' }}>
-      <FontAwesomeIcon icon={props.ico} />
-      <Card.Body>
+    <Card className='infoCard  align-items-center' >
+      <Card.Body className='justify-content-center d-flex flex-column'>
         <Card.Title>{props.name}</Card.Title>
         <Button variant="primary" href={props.link}>{props.action}</Button>
       </Card.Body>
